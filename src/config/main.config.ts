197adca4f62eb30e66ff688cb7ef08baeec4bb53
fachrigans <fachri@gmail.com>
2026-02-23 01:26:34 +0700

    add role guard

--- src/config/main.config.ts
+++ src/config/main.config.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 export const DATABASE_URL = process.env.DATABASE_URL;
+export const JWT_TOKEN_SECRET_KEY = process.env.JWT_TOKEN_SECRET_KEY;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
