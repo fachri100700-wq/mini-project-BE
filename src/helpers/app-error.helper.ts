672c23912febfc5521fc301aab46d21443a60805
fachrigans <fachri@gmail.com>
2026-02-23 06:31:36 +0700

    fix merge conflict

--- src/helpers/app-error.helper.ts
+++ src/helpers/app-error.helper.ts
@@ -3,7 +3,6 @@ export default function AppError(message: string, statusCode: number) {
     statusCode: number;
     expose: boolean;
   };
-  
 
   error.statusCode = statusCode;
   error.expose = true;
