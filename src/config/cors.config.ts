15c1c98e93d4671665ed505aa52623eee7cd8dbc
fachri100700-wq <fachri100700@gmail.com>
2026-06-11 13:09:09 +0700

    fix: cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -4,6 +4,7 @@ const whiteList = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://eventajaaa.vercel.app/",
+  "mini-project-fe-alpha.vercel.app",
   NEXT_URL,
 ].filter(Boolean); // filter out undefined/null values
 
